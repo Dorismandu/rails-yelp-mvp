981bc2bd7b9e349756316245c447fd29741d5f0f
Dorothee Wallois <dorothee.wallois@live.fr>
2021-04-13 21:07:23 +0200

    exo 2 yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "cleaning Restaurant DB"
+Restaurant.destroy_all
+puts "Restaurant DB cleaned!"
+5.times do 
+  my_restaurant = Restaurant.new(
+    name: Faker::Restaurant.name ,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample, 
+  )
+  my_restaurant.save
+end
+
